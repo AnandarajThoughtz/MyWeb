85c6967f0ff0ffb8d2682000e60bf7e8d9e588d2
AnandaRajB <anandraj.baskaran@gmail.com>
2019-06-08 00:39:46 +0530

    #comment added BO project

---
+++ SiteBO/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiteBO
+{
+    public class User
+    {
+        public void CreateUser(String emailID, String pword)
+        {
+
+        }
+    }
+}
